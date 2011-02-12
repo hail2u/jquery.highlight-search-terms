5fc1550ee6e03f7b8c7b0121caf2f7e58fd7c945
Kyo Nagashima <kyo@hail2u.net>
2011-02-12 16:04:07 +0900

    add tagName option

--- jquery.highlight-search-terms.js
+++ jquery.highlight-search-terms.js
@@ -1,5 +1,5 @@
-/*!
- * jQuery Plugin: Highlight Search Terms - version 0.2
+/**
+ * @preserve jQuery Plugin: Highlight Search Terms - version 0.3
  * http://github.com/hail2u/jquery.highlight-search-terms
  * Highlight search terms in referrer URL from Google, Yahoo!, Bing and custom site.
  *
@@ -7,6 +7,7 @@
  * This library licensed under MIT license:
  * http://opensource.org/licenses/mit-license.php
  */
+
 (function ($) {
   $.fn.highlightSearchTerms = function (options) {
     var o = $.extend({}, $.fn.highlightSearchTerms.defaults, options);
@@ -19,7 +20,9 @@
       // Highlight terms
       if (terms !== "") {
         terms = new RegExp("(" + terms + ")", "gi");
-        var highlighted = "<" + encodeEntities(o.tagName) + " class=\"" + encodeEntities(o.className) + "\">$1</em>";
+        var t = encodeEntities(o.tagName);
+        var c = encodeEntities(o.className);
+        var highlighted = "<" + t + " class=\"" + c + "\">$1</" + t + ">";
         this.find(":not(iframe, option, script, textarea)").contents().each(function () {
           if (this.nodeType === 3) {
             var s = encodeEntities(this.nodeValue).replace(terms, highlighted);
